e46ec5468794705f85d57486c5a763c333c0f2af
抄板大王 <1609010708@qq.com>
2025-08-07 20:18:15 +0800

    完善灰度代码25.8.7

--- stm32f407vgt6_template_project/bsp/grayscale.c
+++ stm32f407vgt6_template_project/bsp/grayscale.c
@@ -2,38 +2,62 @@
 
 GrayscaleInitTypeDef Grayscale_InitStructure;
 
+/**
+ * @brief Grayscale初始化
+ *
+ */
 void Grayscale_Init(void)
 {
-    Grayscale_InitStructure.address = 0x48;                   // Device address
-    Grayscale_InitStructure.input_mode = SINGLE_ENDED_MODE;   // Default input mode
-    Grayscale_InitStructure.PD_selection = PD_MODE_ADC_ON;    // Default power-down selection
-    Grayscale_InitStructure.channel = SINGLE_CH0;             // Default channel selection
+    Grayscale_InitStructure.address = DEFAULT_SLAVE_ADDR;     // 从机地址
+    Grayscale_InitStructure.input_mode = SINGLE_ENDED_MODE;   // 输入模式
+    Grayscale_InitStructure.PD_selection = PD_MODE_ADC_ON;    // 电源管理模式
+    Grayscale_InitStructure.channel = SINGLE_CH0;             // 通道选择
 }
 
-// Set the device address
+/**
+ * @brief 设置Grayscale从机地址
+ *
+ * @param address 从机地址
+ */
 void Set_GrayscaleAddress(uint8_t address)
 {
     Grayscale_InitStructure.address = address;
 }
 
-// Set the input mode
+/**
+ * @brief 设置Grayscale输入模式
+ *
+ * @param input_mode 输入模式
+ */
 void Set_GrayscaleInputMode(uint8_t input_mode)
 {
     Grayscale_InitStructure.input_mode = input_mode;
 }
 
-// Set the power-down selection
+/**
+ * @brief 设置Grayscale电源管理模式
+ *
+ * @param PD_selection 电源管理模式
+ */
 void Set_GrayscalePDSelection(uint8_t PD_selection)
 {
     Grayscale_InitStructure.PD_selection = PD_selection;
 }
 
+/**
+ * @brief 设置Grayscale转换通道
+ *
+ * @param channel 所选通道
+ */
 void Set_GrayscaleChannel(uint8_t channel)
 {
     Grayscale_InitStructure.channel = channel;
 }
 
-// 写命令，(内部使用,用户无需更改)
+/**
+ * @brief 发送命令（由上述模式确定）
+ *
+ */
 void Grayscale_SendCommand(void)
 {
     uint8_t command =
@@ -41,7 +65,11 @@ void Grayscale_SendCommand(void)
     HAL_I2C_Master_Transmit(&hi2c3, (Grayscale_InitStructure.address << 1), &command, 1, HAL_MAX_DELAY);
 }
 
-// 读数据，(内部使用,用户无需更改)
+/**
+ * @brief 读取数据
+ *
+ * @return uint16_t 指定通道数据（高4位为0）
+ */
 uint16_t Grayscale_ReadData(void)
 {
     uint8_t data[2] = {0};
@@ -50,53 +78,102 @@ uint16_t Grayscale_ReadData(void)
     return (data[0] << 8) | data[1];
 }
 
-// 通道转换，(内部使用,用户无需更改)
+/**
+ * @brief 通道选择（内部使用）
+ *
+ * @param channel 通道编号
+ * @return uint8_t 通道码
+ */
 uint8_t channel_switch(uint16_t channel)
 {
     switch (channel) {
-        case 0: return SINGLE_CH0;
-        case 1: return SINGLE_CH1;
-        case 2: return SINGLE_CH2;
-        case 3: return SINGLE_CH3;
-        case 4: return SINGLE_CH4;
-        case 5: return SINGLE_CH5;
-        case 6: return SINGLE_CH6;
-        case 7: return SINGLE_CH7;
+        case 0:
+            if (Grayscale_InitStructure.input_mode == SINGLE_ENDED_MODE)
+                return SINGLE_CH0;
+            else
+                return DIFFER_CH0_1;
+        case 1:
+            if (Grayscale_InitStructure.input_mode == SINGLE_ENDED_MODE)
+                return SINGLE_CH1;
+            else
+                return DIFFER_CH2_3;
+        case 2:
+            if (Grayscale_InitStructure.input_mode == SINGLE_ENDED_MODE)
+                return SINGLE_CH2;
+            else
+                return DIFFER_CH4_5;
+        case 3:
+            if (Grayscale_InitStructure.input_mode == SINGLE_ENDED_MODE)
+                return SINGLE_CH3;
+            else
+                return DIFFER_CH6_7;
+        case 4:
+            if (Grayscale_InitStructure.input_mode == SINGLE_ENDED_MODE)
+                return SINGLE_CH4;
+            else
+                return DIFFER_CH1_0;
+        case 5:
+            if (Grayscale_InitStructure.input_mode == SINGLE_ENDED_MODE)
+                return SINGLE_CH5;
+            else
+                return DIFFER_CH3_2;
+        case 6:
+            if (Grayscale_InitStructure.input_mode == SINGLE_ENDED_MODE)
+                return SINGLE_CH6;
+            else
+                return DIFFER_CH5_4;
+        case 7:
+            if (Grayscale_InitStructure.input_mode == SINGLE_ENDED_MODE)
+                return SINGLE_CH7;
+            else
+                return DIFFER_CH7_6;
         default: return 0;
     }
 }
 
-
+/**
+ * @brief 读取指定通道数据
+ *
+ * @param channel 通道编号
+ * @return uint16_t 读取数据
+ */
 uint16_t Grayscale_ReadChannel(uint16_t channel)
 {
-    uint16_t data_buff;
     Set_GrayscaleChannel(channel_switch(channel));
-    data_buff = Grayscale_ReadData();
+    uint16_t data_buff = Grayscale_ReadData();
     return data_buff;
 }
 
-void Grayscale_ReadAllChannels(uint16_t* data_list)
+/**
+ * @brief 读取所有通道数据
+ *
+ * @param data_buff 数据缓冲区
+ */
+void Grayscale_ReadAllChannels(uint16_t* data_buff)
 {
     for (uint16_t i = 0; i < 8; i++) {
-        *(data_list + i) = Grayscale_ReadChannel(i);
+        *(data_buff + i) = Grayscale_ReadChannel(i);
     }
 }
 
-
+/**
+ * @brief Grayscale测试代码，串口打印所有通道数据，使用前务必初始化串口屏幕打印
+ *
+ */
 void test_grayscale(void)
 {
-    uint16_t data_list[8];
+    uint16_t data_buff[8];
     Grayscale_Init();
     while (1) {
-        Grayscale_ReadAllChannels(data_list);
+        Grayscale_ReadAllChannels(data_buff);
         debug_print("%d,%d,%d,%d,%d,%d,%d,%d\n",
-                    data_list[0],
-                    data_list[1],
-                    data_list[2],
-                    data_list[3],
-                    data_list[4],
-                    data_list[5],
-                    data_list[6],
-                    data_list[7]);
+                    data_buff[0],
+                    data_buff[1],
+                    data_buff[2],
+                    data_buff[3],
+                    data_buff[4],
+                    data_buff[5],
+                    data_buff[6],
+                    data_buff[7]);
     }
 }
--- stm32f407vgt6_template_project/bsp/grayscale.h
+++ stm32f407vgt6_template_project/bsp/grayscale.h
@@ -4,23 +4,31 @@
 #include "bsp.h"
 #include "i2c.h"
 
-// Grayscale sensor initialization structure
+/**
+ * @brief Grayscale 初始化结构体
+ *         
+ *        address 从机地址（具体地址由A1、A0引脚确定，详见ADS7828数据手册）
+ *        input_mode 输入模式（单端输入/差分输入）
+ *        PD_selection 电源模式选择
+ *        channel 通道选择
+ */
 typedef struct GrayscaleInit
 {
-    uint8_t address;                         // Device address
-    uint8_t input_mode;                      // Input mode (e.g., single-ended, differential)
-    uint8_t PD_selection;                    // Power-down selection
-    uint8_t channel;                         // Channel selection
+    uint8_t address;                         
+    uint8_t input_mode;                      
+    uint8_t PD_selection;                    
+    uint8_t channel;                        
 }GrayscaleInitTypeDef;
 
+#define DEFAULT_SLAVE_ADDR          0x48     //默认从机地址
 /************input mode definitions************/
-#define DIFFERENTIAL_MODE           0x00     // Differential mode
-#define SINGLE_ENDED_MODE           0x80     // Single-ended mode
+#define DIFFERENTIAL_MODE           0x00     // 差分输入模式
+#define SINGLE_ENDED_MODE           0x80     // 单端输入模式
 /************power-down mode definitions************/
-#define PD_MODE_CONV_SLEEP          0x00     // Conversion sleep, ADC and internal reference off
-#define PD_MODE_ADC_ON              0x04     // ADC on and internal reference off
-#define PD_MODE_REF_ON              0x08     // ADC off and internal reference on
-#define PD_MODE_REF_ADC_ON          0x0C     // ADC and internal reference on
+#define PD_MODE_CONV_SLEEP          0x00     // 休眠模式，ADC、内部参考电压关闭
+#define PD_MODE_ADC_ON              0x04     // ADC开启、内部参考电压关闭
+#define PD_MODE_REF_ON              0x08     // ADC关闭、内部参考电压开启
+#define PD_MODE_REF_ADC_ON          0x0C     // ADC、内部参考电压开启
 /************channel selection definitions************/
 #define SINGLE_CH0                  0x00     // Single channel 0
 #define SINGLE_CH1                  0x40     // Single channel 1
@@ -38,7 +46,6 @@ typedef struct GrayscaleInit
 #define DIFFER_CH3_2                0x50     // Differential channel 2-/3+
 #define DIFFER_CH5_4                0x60     // Differential channel 4-/5+
 #define DIFFER_CH7_6                0x70     // Differential channel 6-/7+
-
 
 void Grayscale_Init(void);
 void Set_GrayscaleAddress(uint8_t address);
